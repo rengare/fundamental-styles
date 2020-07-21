de5f43e62f2a0d12abae5d1c9ab5588ce4935962
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-21 14:49:05 +0200

    test

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
 set -e
-
+cat package.json
 git status
 
 npm run std-version -- --prerelease rc --no-verify --debug
 
 git status
-git push --follow-tags origin master
+
+cat package.json
 #build dist and component folders
