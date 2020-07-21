fe519fbaadaa034a95701f0ec5549d35bf1b164c
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-21 13:16:32 +0200

    test2

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-git config --global user.email "fundamental@sap.com"
-git config --global user.name "fundamental-bot"
-
 git status
 
 npm run std-version -- --prerelease rc --no-verify --debug
