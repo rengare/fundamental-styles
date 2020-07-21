3d66293701ff0916ba4e9ad227437bad51adb022
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-21 15:34:19 +0200

    t

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -2,7 +2,7 @@
 
 set -e
 git status
-npx standard-version --release-as prerelease rc --no-verify
+npx standard-version --prerelease rc --no-verify
 #npm run std-version -- --prerelease rc --no-verify --debug
 
 git status
