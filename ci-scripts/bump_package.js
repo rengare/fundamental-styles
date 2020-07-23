4ca837b74a8d989762272e5996f79be9b460ba48
gorecki.gracjan <gorecki.gracjan@hotmail.com>
2020-07-23 11:05:24 +0200

    test5

--- ci-scripts/bump_package.js
+++ ci-scripts/bump_package.js
@@ -1,16 +1,20 @@
-let fs = require('fs');
+const fs = require('fs');
 
-let dots =  fs.existsSync('./package.json')? '.' : '..';
+let dots = fs.existsSync('./package.json') ? '.' : '..';
 
-let packageContent = JSON.parse(fs.readFileSync(`${dots}/package.json`, 'utf8'));
-let versionContent = JSON.parse(fs.readFileSync(`${dots}/version.json`, 'utf8'));
+let version = process.argv[2];
 
-packageContent.version = 1
+if (version) {
+    version = version.replace('v', '');
+    
+    let packageContent = JSON.parse(fs.readFileSync(`${dots}/package.json`, 'utf8'));
+    packageContent.version = version;
 
-if(fs.existsSync(`${dots}/package-lock.json`)){
-    let lockContent = JSON.parse(fs.readFileSync(`${dots}/package-lock.json`, 'utf8'));
-    lockContent.version = versionContent.version;
-    fs.writeFileSync(`${dots}/package-lock.json`, JSON.stringify(lockContent, null, '\t'));
+    if (fs.existsSync(`${dots}/package-lock.json`)) {
+        let lockContent = JSON.parse(fs.readFileSync(`${dots}/package-lock.json`, 'utf8'));
+        lockContent.version = version;
+        fs.writeFileSync(`${dots}/package-lock.json`, JSON.stringify(lockContent, null, '\t'));
+    }
+
+    fs.writeFileSync(`${dots}/package.json`, JSON.stringify(packageContent, null, '\t'));
 }
-
-fs.writeFileSync(`${dots}/package.json`, JSON.stringify(packageContent, null, '\t'));
--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
-# npx standard-version --prerelease rc --no-verify
+std_ver=$(npm run std-version -- --prerelease rc --no-verify)
+release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
 
-npx node ci-scripts/bump_package.js
+npx node ci-scripts/bump_package.js $release_tag
 
 cat package.json | grep version
